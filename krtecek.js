e9ba0320b37738c010e7d5cfa78c8984b0cab198
Vojtěch Zatloukal <voj.zatloukal@seznam.cz>
2021-03-16 00:47:18 +0100

    finální úpravy

--- krtecek.js
+++ krtecek.js
@@ -6,15 +6,21 @@ window.onscroll = (e) => {
     for (let i of el) {
         if (i.classList.contains("layer-man")) {
             console.log("auticko")
-            i.style.transform = "translateX(-" + window.scrollY/3 + "px)  translateY(-" + window.scrollY  + "px)";
+            i.style.transform = "translateX(-" + window.scrollY/3 + "px)  translateY(-" + window.scrollY*1.1  + "px)";
             
         }
         if(i.classList.contains("layer-4")){
             i.style.transform = "translateY(-" + window.scrollY + "px)";
         }
+        if(i.classList.contains("layer-2")){
+            i.style.transform = "translateY(-" + window.scrollY + "px)";
+        }
+        if(i.classList.contains("layer-1")){
+            i.style.transform = "translateY(-" + window.scrollY*0.3 + "px)";
+        }
     
     }  
-    
+
     
 }
 
