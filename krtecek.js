7a559e6c50071e3be1a44c9b0334f35626b7301b
Vojtěch Zatloukal <voj.zatloukal@seznam.cz>
2021-03-15 13:14:38 +0100

    krtecek hotovo vol.1

---
+++ krtecek.js
@@ -0,0 +1,19 @@
+var el = document.getElementsByClassName("parallax-layer")
+
+window.onscroll = (e) => {
+
+
+    for (let i of el) {
+        if (i.classList.contains("layer-man")) {
+            console.log("auticko")
+            i.style.transform = "translateX(-" + window.scrollY/3 + "px)  translateY(-" + window.scrollY  + "px)";
+            
+        }
+        if(i.classList.contains("layer-4")){
+            i.style.transform = "translateY(-" + window.scrollY + "px)";
+        }
+    
+    }
+    
+}
+
